353e3ce1f3f6db71079cd6420132d05037f7dbcc
shlomoPearl <shlomoper94@gmail.com>
2024-05-01 21:39:21 +0300

    chage it to Object Gmail with two method

--- ElectricBill/gmail.py
+++ ElectricBill/gmail.py
@@ -2,80 +2,83 @@ from __future__ import print_function
 
 import base64
 import os.path
+from datetime import datetime, timedelta
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
-# If modifying these scopes, delete the file token.json.
 from ElectricBill.bill import read_bill
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-global count
-count = 0
+address_search = "gviyarevava@revava.co.il"
 
 
-def connect_to_gmail():
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    return creds
+# If modifying these scopes, delete the file token.json.
 
 
-def search_mail(mail_address):
-    query = f"from:{mail_address}"
-    creds = connect_to_gmail()
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        results = service.users().messages().list(userId='me', q=query).execute()
-        if 'messages' in results:
+def parse_date(date_str):
+    return datetime.strptime(date_str, '%d/%m/%Y')
+
+
+class Gmail:
+
+    def __init__(self, address="", result_num=None, date_range=[]):
+        self.address = address
+        self.result_num = result_num
+        self.date_range = [parse_date(date_range[0]).isoformat() + 'Z',
+                           (parse_date(date_range[1]) + timedelta(days=1)).isoformat() + 'Z']
+        self.token_file = "token.json"
+        self.SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+        self.credentials_file = "credentials.json"
+
+    def authenticate(self):
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        creds = None
+        if os.path.exists(self.token_file):
+            creds = Credentials.from_authorized_user_file(self.token_file, self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(self.credentials, self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(self.token_file, "w") as token:
+                token.write(creds.to_json())
+        return creds
+
+    def search_mail(self, creds):
+        attachment_data_list = []
+        try:
+            # Call the Gmail API
+            service = build("gmail", "v1", credentials=creds)
+            results = service.users().messages().list(userId="me",
+                                                      maxResults=36 if self.result_num is None else self.result_num,
+                                                      q=f"from:{self.address} "
+                                                        f"after:{self.date_range[0]} "
+                                                        f"before:{self.date_range[1]}").execute()
             for message in results['messages']:
                 msg = service.users().messages().get(userId='me', id=message['id']).execute()
-                email_subject = ""
+                # get the subject and the date
+                # print(msg['payload']['parts'])
                 for header in msg['payload']['headers']:
-                    if header['name'] == 'Subject':
-                        email_subject = header['value']
-                        break
-                date = ""
-                for char in email_subject:
-                    if char.isdigit() or char == '/':
-                        date += char
+                    if header['name'] == 'Date':
+                        date_time_list = header['value'].split(' ')
+                        date_time = f"{date_time_list[1]}/{date_time_list[2]}/{date_time_list[3]}"
+                        print(datetime.strptime(date_time, "%d/%b/%Y").date().strftime("%d/%m/%Y"))
+
                 # Check for attachments
                 for part in msg['payload']['parts']:
                     if part['filename'] and part['filename'].endswith('.pdf'):
                         attachment_data = part['body']['attachmentId']
-                        attachment = service.users().messages().attachments().get(
-                            userId='me', messageId=message['id'], id=attachment_data).execute()
+                        attachment = service.users().messages().attachments() \
+                            .get(userId='me', messageId=message['id'], id=attachment_data).execute()
                         data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
-                        # Now you can save or process the PDF file
-                        with open('attachment.pdf', 'wb') as f:
-                            f.write(data)
-                        read_bill('attachment.pdf', date)
-                        # You can read the PDF content using a PDF parsing library here
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-
-
-def main():
-    search_mail("gviyarevava@revava.co.il")
-
-
-if __name__ == '__main__':
-    main()
+                        attachment_data_list.append(data)
+                return attachment_data_list
+        except HttpError as error:
+            print(f"An error occurred: {error}")
