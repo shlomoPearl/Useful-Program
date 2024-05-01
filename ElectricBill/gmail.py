e9d1b616642b06b7c7835e851385960fb581df73
shlomoPearl <shlomoper94@gmail.com>
2024-05-01 23:55:02 +0300

    add dict of date as key and attachment data as value and add optional key_word to read only mails with this word in their subject (in case i want only one kinf of bill but get two bills from the same address)

--- ElectricBill/gmail.py
+++ ElectricBill/gmail.py
@@ -9,7 +9,6 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from ElectricBill.bill import read_bill
 
 address_search = "gviyarevava@revava.co.il"
 
@@ -18,17 +17,17 @@ address_search = "gviyarevava@revava.co.il"
 
 
 def parse_date(date_str, strp, date_format):
-    if strp:                               # '%d/%m/%Y'
+    if strp:  # '%d/%m/%Y'
         return datetime.strptime(date_str, date_format)
     else:
         return date_str.strftime(date_format)
 
 
-
 class Gmail:
 
-    def __init__(self, address="", result_num=None, date_range=[]):
+    def __init__(self, address="", key_word=None, result_num=None, date_range=[]):
         self.address = address
+        self.key_word = key_word
         self.result_num = result_num
         self.date_range = [parse_date(date_range[0], True, '%d/%m/%Y').isoformat() + 'Z',
                            (parse_date(date_range[1], True, '%d/%m/%Y') + timedelta(days=1)).isoformat() + 'Z']
@@ -65,28 +64,33 @@ class Gmail:
                                                       q=f"from:{self.address} "
                                                         f"after:{self.date_range[0]} "
                                                         f"before:{self.date_range[1]}").execute()
-            attachment_data_list = []
+            # make it dict to take always the update bill
+            date_attachment_dict = {}
             for message in results['messages']:
 
                 msg = service.users().messages().get(userId='me', id=message['id']).execute()
                 # get the subject and the date
                 # print(msg['payload']['parts'])
-                date = ""
-                for header in msg['payload']['headers']:
-                    if header['name'] == 'Date':
-                        date_time_list = header['value'].split(' ')
-                        date_time = f"{date_time_list[1]}/{date_time_list[2]}/{date_time_list[3]}"
-                        # print(datetime.strptime(date_time, "%d/%b/%Y").date().strftime("%d/%m/%Y"))
-                        date = parse_date(parse_date(date_time,True,"%d/%b/%Y").date(),False,"%d/%m/%Y")
-                        print(date)
-                # Check for attachments
+
+                # Check for attachments pdf files and if it contains the key_word
                 for part in msg['payload']['parts']:
                     if part['filename'] and part['filename'].endswith('.pdf'):
-                        attachment_data = part['body']['attachmentId']
-                        attachment = service.users().messages().attachments() \
-                            .get(userId='me', messageId=message['id'], id=attachment_data).execute()
-                        data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
-                        attachment_data_list.append(data)
-                return attachment_data_list
+                        for header in msg['payload']['headers']:
+                            if header['name'] == 'Subject':
+                                email_subject = header['value']
+                                if self.key_word is None or self.key_word in email_subject:
+                                    attachment_data = part['body']['attachmentId']
+                                    attachment = service.users().messages().attachments() \
+                                        .get(userId='me', messageId=message['id'], id=attachment_data).execute()
+                                    data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
+                                    if header['name'] == 'Date':
+                                        date_time_list = header['value'].split(' ')
+                                        date_time = f"{date_time_list[1]}/{date_time_list[2]}/{date_time_list[3]}"
+                                        # print(datetime.strptime(date_time, "%d/%b/%Y").date().strftime("%d/%m/%Y"))
+                                        date = parse_date(parse_date(date_time, True, "%d/%b/%Y").date(), False,
+                                                          "%d/%m/%Y")
+                                        print(date)
+                                        date_attachment_dict[date] = data
+            return date_attachment_dict
         except HttpError as error:
             print(f"An error occurred: {error}")
