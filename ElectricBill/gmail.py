5167e42816b18287157f9e4cdae31812b3e31099
shlomoPearl <shlomoper94@gmail.com>
2024-05-08 12:19:10 +0300

    change the date search into the query and add function for that

--- ElectricBill/gmail.py
+++ ElectricBill/gmail.py
@@ -3,14 +3,12 @@ from __future__ import print_function
 import base64
 import os.path
 from datetime import datetime, timedelta
-
+import calendar
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
-address_search = "gviyarevava@revava.co.il"
 
 
 # If modifying these scopes, delete the file token.json.
@@ -20,17 +18,46 @@ def parse_date(date_str, strp, date_format):
     if strp:  # '%d/%m/%Y'
         return datetime.strptime(date_str, date_format)
     else:
-        return date_str.strftime(date_format)
+        return datetime.strftime(date_str, date_format)
+
+
+def increment_date(date_str):
+    date_object = datetime.strptime(date_str, '%d/%m/%Y')
+    day_obj = date_object.day
+    month_obj = date_object.month
+    next_month = (month_obj + 1) % 12 if (month_obj + 1) % 12 > 0 else 12
+    year_obj = date_object.year
+    num_day_current = calendar.monthrange(year_obj, month_obj)[1]
+    num_day_next = calendar.monthrange(year_obj, next_month)[1]
+    new_date = date_object + timedelta(days=num_day_next if day_obj == num_day_current else num_day_current)
+    new_date_str = new_date.strftime('%d/%m/%Y')
+    return new_date_str
+
+
+def decrement_date(date_str):
+    date_object = datetime.strptime(date_str, '%d/%m/%Y')
+    day_obj = date_object.day
+    month_obj = date_object.month
+    before_month = (month_obj - 1) % 12 if (month_obj - 1) % 12 > 0 else 12
+    year_obj = date_object.year
+    num_day_current = calendar.monthrange(year_obj, month_obj)[1]
+    num_day_before = calendar.monthrange(year_obj, before_month)[1]
+    new_date = date_object + timedelta(days=-(num_day_current if day_obj == num_day_current else num_day_before))
+    new_date_str = new_date.strftime('%d/%m/%Y')
+    return new_date_str
 
 
 class Gmail:
 
-    def __init__(self, address="", key_word=None, result_num=None, date_range=[]):
+    def __init__(self, address=None, subject=None, filename='pdf', key_word=None, result_num=36, date_range=[]):
         self.address = address
+        self.subject = subject
+        self.filename = filename
         self.key_word = key_word
         self.result_num = result_num
-        self.date_range = [parse_date(date_range[0], True, '%d/%m/%Y').isoformat() + 'Z',
-                           (parse_date(date_range[1], True, '%d/%m/%Y') + timedelta(days=1)).isoformat() + 'Z']
+        self.date_range = date_range
+        # self.date_range = [parse_date(date_range[0], True, '%d/%m/%Y').isoformat() + 'Z',
+        #                    (parse_date(date_range[1], True, '%d/%m/%Y') + timedelta(days=1)).isoformat() + 'Z']
         self.token_file = "token.json"
         self.SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
         self.credentials_file = "credentials.json"
@@ -52,45 +79,45 @@ class Gmail:
             # Save the credentials for the next run
             with open(self.token_file, "w") as token:
                 token.write(creds.to_json())
+        print("The authentication was successful")
         return creds
 
     def search_mail(self, creds):
-
+        query = f"from:{self.address} subject:{self.subject} filename:{self.filename}" \
+                f" after:{increment_date(self.date_range[0])} before:{increment_date(self.date_range[1])}"
+        print(query)
         try:
             # Call the Gmail API
             service = build("gmail", "v1", credentials=creds)
-            results = service.users().messages().list(userId="me",
-                                                      maxResults=36 if self.result_num is None else self.result_num,
-                                                      q=f"from:{self.address} "
-                                                        f"after:{self.date_range[0]} "
-                                                        f"before:{self.date_range[1]}").execute()
+            results = service.users().messages().list(userId="me", maxResults=self.result_num, q=query).execute()
             # make it dict to take always the update bill
             date_attachment_dict = {}
             for message in results['messages']:
 
                 msg = service.users().messages().get(userId='me', id=message['id']).execute()
                 # get the subject and the date
-                # print(msg['payload']['parts'])
-
                 # Check for attachments pdf files and if it contains the key_word
                 for part in msg['payload']['parts']:
                     if part['filename'] and part['filename'].endswith('.pdf'):
+                        attachment_data = part['body']['attachmentId']
+                        attachment = service.users().messages().attachments() \
+                            .get(userId='me', messageId=message['id'], id=attachment_data).execute()
+                        data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
                         for header in msg['payload']['headers']:
                             if header['name'] == 'Subject':
                                 email_subject = header['value']
-                                if self.key_word is None or self.key_word in email_subject:
-                                    attachment_data = part['body']['attachmentId']
-                                    attachment = service.users().messages().attachments() \
-                                        .get(userId='me', messageId=message['id'], id=attachment_data).execute()
-                                    data = base64.urlsafe_b64decode(attachment['data'].encode('UTF-8'))
-                                    if header['name'] == 'Date':
-                                        date_time_list = header['value'].split(' ')
-                                        date_time = f"{date_time_list[1]}/{date_time_list[2]}/{date_time_list[3]}"
-                                        # print(datetime.strptime(date_time, "%d/%b/%Y").date().strftime("%d/%m/%Y"))
-                                        date = parse_date(parse_date(date_time, True, "%d/%b/%Y")
-                                                          .date(), False, "%m/%Y")
-                                        print(date)
-                                        date_attachment_dict[date] = data
+                                # if self.key_word is None or self.key_word in email_subject:
+                                #     continue
+                                print(email_subject)
+                            if header['name'] == 'Date':
+                                date_time_list = header['value'].split(' ')
+                                date_time = f"{date_time_list[1]}/{date_time_list[2]}/{date_time_list[3]}"
+                                d_with_month_n = parse_date(date_time, True, "%d/%b/%Y").date()
+                                s_d_with_month_n = parse_date(d_with_month_n, False, "%d/%m/%Y")
+                                real_date_bill = decrement_date(s_d_with_month_n)
+                                o_real_date_bill = parse_date(real_date_bill,True, "%d/%m/%Y")
+                                date = parse_date(o_real_date_bill, False, "%m/%Y")
+                                date_attachment_dict[date] = data
             return date_attachment_dict
         except HttpError as error:
             print(f"An error occurred: {error}")
