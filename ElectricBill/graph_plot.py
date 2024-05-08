2a7218176879e000754f0cd39fae173b8f0bf6cf
shlomoPearl <shlomoper94@gmail.com>
2024-05-08 13:43:03 +0300

    add values above the bar and option to insert title to the constructor

--- ElectricBill/graph_plot.py
+++ ElectricBill/graph_plot.py
@@ -1,17 +1,25 @@
+from datetime import datetime
 import matplotlib.pyplot as plt
 
 class PatymentGraph:
-    def __init__(self, bill_dict):
+    def __init__(self, bill_dict, title='Payment account graph'):
         self.bill_dict = bill_dict
+        self.title = title
 
     def plot_graph(self):
-        dates = list(self.bill_dict.keys())
-        values = list(self.bill_dict.values())
-        plt.bar(dates, values)
-        plt.xticks(rotation=45)  # Rotate the x-axis labels for better readability
+        dates = [datetime.strptime(date, '%m/%Y') for date in self.bill_dict.keys()]
+        sorted_dates = sorted(dates)
+        sorted_dates_lst = [date.strftime('%m/%Y') for date in sorted_dates]
+        # Get corresponding values in the sorted order
+        values = [self.bill_dict[date] for date in sorted_dates_lst]
+
+        for i, value in enumerate(values):
+            plt.text(i, value + 5, str(value), ha='center', fontsize=8)
+        plt.bar(sorted_dates_lst, values)
+        plt.xticks(rotation=40)  # Rotate the x-axis labels for better readability
         plt.xlabel('Date')
         plt.ylabel('Paid')
-        plt.title('Payment account graph')
+        plt.title(self.title)
         plt.grid(False)
         plt.show()
 
