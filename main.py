7fdfbb5723f1e073a36bc8d71e1fbe5c0bd41246
shlomoPearl <shlomoper94@gmail.com>
2025-06-11 15:30:41 +0300

    start with postgresql

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import io
 import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 @app.route("/", methods=["GET", "POST"])
